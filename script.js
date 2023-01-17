9e59f514b8cf21bfb236f8fc8b70843a4e186a23
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-01-17 16:27:37 +0530

    added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your code here
+// first create an array of image
+
+let arr = ["img1","img2","img3","img4","img5"];
+
+
+
+function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
+console.log(shuffleArray(arr))
